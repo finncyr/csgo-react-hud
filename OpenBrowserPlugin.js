98902d0d8282f298d88cf8624c1a29a13d79a234
Restyled.io <commits@restyled.io>
2021-04-25 08:11:08 +0000

    Restyled by prettier

--- OpenBrowserPlugin.js
+++ OpenBrowserPlugin.js
@@ -1,17 +1,19 @@
-const open = require('open');
+const open = require("open");
 
 module.exports = class OpenBrowser {
   constructor(options) {
-    if (typeof options === 'string') {
-      this.options = Object.assign({hasOpen : false}, {url : options});
+    if (typeof options === "string") {
+      this.options = Object.assign({ hasOpen: false }, { url: options });
     } else {
-      this.options = Object.assign({
-        port : 8080,
-        host : 'localhost',
-        protocol : 'http:',
-        hasOpen : false
-      },
-                                   options);
+      this.options = Object.assign(
+        {
+          port: 8080,
+          host: "localhost",
+          protocol: "http:",
+          hasOpen: false,
+        },
+        options
+      );
     }
   }
 
@@ -19,23 +21,19 @@ module.exports = class OpenBrowser {
     const options = this.options;
     let url;
     let hasOpen = options.hasOpen;
-    if (options.protocol && !options.protocol.endsWith(':'))
-      options.protocol += ':';
-    if (options.url)
-      url = options.url;
-    else
-      url = `${options.protocol}//${options.host}:${options.port}`;
+    if (options.protocol && !options.protocol.endsWith(":"))
+      options.protocol += ":";
+    if (options.url) url = options.url;
+    else url = `${options.protocol}//${options.host}:${options.port}`;
     if (compiler.hooks) {
-      compiler.hooks.afterEmit.tap('openBrowser', () => {
-        if (!hasOpen)
-          open(url);
+      compiler.hooks.afterEmit.tap("openBrowser", () => {
+        if (!hasOpen) open(url);
         hasOpen = true;
         this.options.hasOpen = true;
       });
     } else {
-      compiler.plugin('after-emit', (c, cb) => {
-        if (!hasOpen)
-          open(url);
+      compiler.plugin("after-emit", (c, cb) => {
+        if (!hasOpen) open(url);
         hasOpen = true;
         this.options.hasOpen = true;
         return cb();
